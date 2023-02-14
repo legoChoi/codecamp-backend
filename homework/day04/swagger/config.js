13334b0c5a1bfb0901a493554b6754b4edd076a6
legoChoi <chlx159@gmail.com>
2023-02-14 22:08:16 +0900

    codecamp homework #04

--- homework/day04/swagger/config.js
+++ homework/day04/swagger/config.js
@@ -2,7 +2,7 @@ export const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "나만의 미니프로젝트 API 명세서",
+      title: "day04 homework API 명세서",
       version: "1.0.0",
     },
   },
