9d594fd78eb54540d5d963e866547456aed4c1d6
legoChoi <chlx159@gmail.com>
2023-03-03 17:59:17 +0900

    codecamp login authentication

---
+++ class/20-02-login-authentication/src/apis/auth/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '../users/entities/user.entity';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService, //
+  ) {}
+
+  getAccessToken({ user }) {
+    return this.jwtService.sign(
+      { email: user.email, sub: user.id }, //
+      { secret: 'myAccessKey', expiresIn: '1h' },
+    );
+  }
+}
