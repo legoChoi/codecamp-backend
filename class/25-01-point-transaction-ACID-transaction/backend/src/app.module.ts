b16a6564e599adb7c23370c656f16365f5325e01
legoChoi <chlx159@gmail.com>
2023-03-07 18:15:43 +0900

    codecamp ACID transaction

---
+++ class/25-01-point-transaction-ACID-transaction/backend/src/app.module.ts
@@ -0,0 +1,38 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './apis/auth/auth.module';
+import { BoardModule } from './apis/boards/boards.module';
+import { PointTransactionModule } from './apis/pointTransaction/pointTransaction.module';
+import { ProductCategoryModule } from './apis/productCategory/productCategory.module';
+import { ProductModule } from './apis/products/product.module';
+import { UserModule } from './apis/users/user.module';
+
+@Module({
+  imports: [
+    BoardModule,
+    ProductModule,
+    ProductCategoryModule,
+    UserModule,
+    PointTransactionModule,
+    AuthModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/commons/graphql/schema.gql',
+      context: ({ req, res }) => ({ req, res }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: '0100',
+      database: 'codecamp_project',
+      entities: [__dirname + '/apis/**/*.entity.*'], // entity.ts로 끝나는 모든 파일
+      synchronize: true, // entity와 테이블 동기화
+      logging: true, // 쿼리문 로그 띄워 줌
+    }),
+  ],
+})
+export class AppModule {}
