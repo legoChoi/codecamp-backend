db618203cbdb9e3600eb96f48c5eb0f54f10d280
legoChoi <chlx159@gmail.com>
2023-03-10 16:37:11 +0900

    codecamp elasticsearch

---
+++ class/31-01-elasticsearch-autocomplete/backend/src/apis/pointTransaction/entities/pointTransaction.entity.ts
@@ -0,0 +1,48 @@
+import { Field, Int, ObjectType, registerEnumType } from '@nestjs/graphql';
+import { User } from 'src/apis/users/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+export enum POINT_TRANSACTION_STATUS_ENUM {
+  PAYMENT = 'PAYMENT',
+  CANCEL = 'CANCEL',
+}
+
+registerEnumType(POINT_TRANSACTION_STATUS_ENUM, {
+  name: 'POINT_TRANSACTION_STATUS_ENUM',
+});
+
+// 포인트 테이블은 인서트 온리 테이블
+// 히스토리 추적을 위해 인서트만 허용 => 수정일 데이터는 필요없음
+@Entity()
+@ObjectType()
+export class PointTransaction {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => String)
+  id: string;
+
+  @Column()
+  @Field(() => String)
+  impUid: string;
+
+  @Column()
+  @Field(() => Int)
+  amount: number;
+
+  @Column({ type: 'enum', enum: POINT_TRANSACTION_STATUS_ENUM })
+  @Field(() => POINT_TRANSACTION_STATUS_ENUM)
+  status: string;
+
+  @ManyToOne(() => User)
+  @Field(() => User)
+  user: User;
+
+  @CreateDateColumn()
+  @Field(() => Date)
+  createdAt: Date;
+}
