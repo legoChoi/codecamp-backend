4d125bf192eb6346fbe8179c3c684163082adeb9
legoChoi <chlx159@gmail.com>
2023-03-16 16:40:36 +0900

    codecamp homework day18 menu query

--- homework/day18/src/apis/menu/menu.resolver.ts
+++ homework/day18/src/apis/menu/menu.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateMenuInput } from './dto/createMenu.input';
 import { Menu } from './entities/menu.entity';
 import { MenuService } from './menu.service';
@@ -6,6 +6,18 @@ import { MenuService } from './menu.service';
 @Resolver()
 export class MenuResolver {
   constructor(private readonly menuService: MenuService) {}
+
+  @Query(() => Menu)
+  fetchMenu(
+    @Args('id') id: string, //
+  ) {
+    return this.menuService.find({ id });
+  }
+
+  @Query(() => [Menu])
+  fetchMenus() {
+    return this.menuService.findAll();
+  }
 
   @Mutation(() => Menu)
   createMenu(
