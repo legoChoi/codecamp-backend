3a783c5ca1c8a8be01f26a246dc9ba2e1e160607
legoChoi <chlx159@gmail.com>
2023-03-18 23:03:11 +0900

    codecamp homework day 18 point

--- homework/day18/src/apis/point/point.resolver.ts
+++ homework/day18/src/apis/point/point.resolver.ts
@@ -1,12 +1,16 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { PlusPointInput } from './dto/plusPoint.input';
 import { PointService } from './point.service';
 
 @Resolver()
 export class PointResolver {
   constructor(private readonly pointService: PointService) {}
 
-  @Query(() => String)
-  getPointTest() {
-    return 'point test';
+  @Mutation(() => String)
+  plusPoint(
+    @Args('pointValue') pointValue: number, //
+    @Args('userId') userId: string,
+  ) {
+    return this.pointService.plus({ pointValue, userId });
   }
 }
