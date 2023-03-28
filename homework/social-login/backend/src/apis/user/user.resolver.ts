fca39ccbd0dcdee5bc4af2c14f7de288495e3ec3
legoChoi <chlx159@gmail.com>
2023-03-29 06:31:31 +0900

    social-login user apis

--- homework/social-login/backend/src/apis/user/user.resolver.ts
+++ homework/social-login/backend/src/apis/user/user.resolver.ts
@@ -1,5 +1,7 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { User } from './entities/user.entity';
 import { UserService } from './user.service';
+import * as bcrypt from 'bcrypt';
 
 @Resolver()
 export class UserResolver {
@@ -10,5 +12,22 @@ export class UserResolver {
   @Query(() => String)
   getHello() {
     return this.userService.getHello();
+  }
+
+  @Mutation(() => User)
+  async createUser(
+    @Args('userId') userId: string,
+    @Args('pwd') pwd: string,
+    @Args('nick') nick: string,
+    @Args('phone') phone: string,
+  ) {
+    const hashedPassword = await bcrypt.hash(pwd, 10); // 비밀번호 bcrypt 해싱
+
+    return this.userService.create({
+      userId,
+      hashedPassword,
+      nick,
+      phone,
+    });
   }
 }
