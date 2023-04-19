de939af1dea5fd3fd4308d2525136b023ce27b3e
legoChoi <chlx159@gmail.com>
2023-04-19 13:10:11 +0900

    connect mysql

--- nestjs-with-rest/pract-project/src/main.ts
+++ nestjs-with-rest/pract-project/src/main.ts
@@ -5,6 +5,6 @@ import { HttpExceptionFilter } from './cats/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
